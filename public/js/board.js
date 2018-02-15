58e4e5bf0f4b1efd57c3411c01d9e1895d5775b2
Neeraj Jaiswal <neerajj85@gmail.com>
2018-02-15 17:12:16 +0530

    [#5] Madhuri/Neeraj - Added place for activity log and player cards
    What changes have you made?
    
    * Added place on page to show player cards
    * Added place on page to show activity log
    
    Why do we need this change?
    
    * To show the player his cards
    * to update all the players about game activity
    
    What can this change impact?

---
+++ public/js/board.js
@@ -0,0 +1,12 @@
+let sendAjaxRequest = function (method, url, cb) {
+  let req = new XMLHttpRequest();
+  req.open(method, url);
+  req.addEventListener('load',() => cb(req.responseText));
+  req.send();
+}
+
+window.onload = function () {
+  sendAjaxRequest('get','/svg/board.svg',(res)=>{
+    document.querySelector('#board').innerHTML = res;
+  })
+}
