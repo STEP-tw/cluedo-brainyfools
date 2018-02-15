5f458c724bcdfecbb94c40dd73121aa9ee85f8f8
omkar mote <omkarmote210@gmail.com>
2018-02-15 18:45:45 +0530

    [#4] omkar/suyog - now we can add player to the game with character
    What changes have you made?
    
    * added post handler to get player name
    * have given different id's to each player
    
    Why do we need this change?
    
    * to add players in game
    
    What can this change impact?
    * it may cause merge conflict in app.js

--- src/models/game.js
+++ src/models/game.js
@@ -6,10 +6,11 @@ class Game {
   constructor(numberOfPlayers) {
     this.numberOfPlayers = numberOfPlayers;
     this.players = {};
-    this.count=1;
+    this.playerCount=0;
   }
   addPlayer(name,id){
-    let character = characterData[this.count++];
+    let character = characterData[++this.playerCount];
+    character=new Character(character,this.playerCount);
     let player = new Player(name,character);
     this.players[id] = player;
   }
