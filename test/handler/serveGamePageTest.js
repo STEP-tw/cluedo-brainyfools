f8ce59a54db37e49dcc3fb2097e141e572ae57c2
Pranav Bansod <pranav.bansod.14@gmail.com>
2018-02-19 18:21:29 +0530

    [#23] Pranav/Madhuri - Added more test for serveGamePage Handler
    What changes have you made?
    
    * added a check while serving gameBoard page
    
    Why do we need this change?
    
    * so that a should not start if it's already started

---
+++ test/handler/serveGamePageTest.js
@@ -0,0 +1,58 @@
+const assert = require('chai').assert;
+const request = require('supertest');
+const app = require('../../app.js');
+
+describe('gameHandlers', function () {
+  beforeEach(function (done) {
+    request(app)
+      .post('/game/new')
+      .send('numberOfPlayers=3')
+      .expect(302)
+      .redirectsTo('/game/join/1234')
+      .end(done);
+  });
+
+  describe('POST /game/join/1234', () => {
+    it('should not allow 4th player to join a 3 player game', function(done){
+      let playerId = 0;
+      app.idGenerator = () => {
+        return ++playerId;
+      };
+      let gameId = '1234';
+      let message = 'All players have already joined in Game';
+      //Adding 1st Player
+      request(app).post('/game/join/1234').send("name=omkar")
+        .redirectsTo('/game/1234/wait')
+        .cookie.include('playerId', '1;')
+        .end(()=>{
+        //Adding 2nd Player
+          request(app).post('/game/join/1234').send("name=Pranav")
+            .redirectsTo('/game/1234/wait')
+            .cookie.include('playerId', '2;')
+            .end(()=>{
+              //Adding 3rd Player
+              request(app).post('/game/join/1234').send("name=Patel")
+                .redirectsTo('/game/1234/wait')
+                .cookie.include('playerId', '3;')
+                .end(()=>{
+                  //get gameBoard Page (Game will start here)
+                  request(app)
+                    .get('/game/1234')
+                    .set('cookie',['playerId=3'])
+                    .body.include('<div id="title">Activity Log</div>')
+                    .expect(200)
+                    .end(()=>{
+                      // Game should not start again
+                      request(app)
+                        .get('/game/1234')
+                        .set('cookie',['playerId=3'])
+                        .body.include('<div id="title">Activity Log</div>')
+                        .expect(200)
+                        .end(done);
+                    });
+                });
+            });
+        });
+    });
+  });
+});
