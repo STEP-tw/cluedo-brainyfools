de7f8d1e0a7560cc966b16ced52510cfe687dd92
pavani <pavanikitty@gmail.com>
2018-03-05 18:50:10 +0530

    [#62] Bhanu/Pavani - changed the back option in selecting suspicion/accusation .
    What changes have you made?
    
    * added back option.
    
    Why do we need this change?
    
    *
    
    What can this change impact?

--- public/js/boardStatus.js
+++ public/js/boardStatus.js
@@ -78,7 +78,9 @@ const showCards = function(suspect){
   });
   let charImages = getImages(characterCards);
   let weaponImages = getImages(weaponCards);
-  document.querySelector('#activity-box').innerHTML = `<div class="popup">
+  document.querySelector('#activity-box').innerHTML = `<div class="back-button">
+  <span onclick="getCurrentPlayer()">&#x226a;</span></div>
+  <div class="popup">
   <div class='cards'>Select a character</div>
   <div id="character"> ${charImages}</div>
   <div class='cards'>Select a weapon</div>
